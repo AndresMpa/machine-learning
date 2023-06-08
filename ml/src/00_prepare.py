d98319599b17dbc88dc8861cda88a0ff49e03963
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-08 16:05:47 -0500

    Using Nest like approach instead of "usual"

--- ml/src/00_prepare.py
+++ ml/src/00_prepare.py
@@ -1,15 +1,16 @@
 from sklearn.datasets import make_circles
+from utilities import handle_directory
+from params import DATA_DIR
 import numpy as np
 import os
 
-OUTPUT_DIR = 'data/'
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+handle_directory(DATA_DIR)
 
 # To generated equivalent data under a seed
 np.random.seed(0)
 
-x_inputs, y_spected = make_circles(n_samples=200, noise=0.05)
+x_inputs, y_expected = make_circles(n_samples=200, noise=0.05)
 
 # Storage data as numpy arrays
-np.save('data/inputData.npy', x_inputs)
-np.save('data/outputData.npy', y_spected)
+np.save(f"{DATA_DIR}inputData.npy", x_inputs)
+np.save(f"{DATA_DIR}outputData.npy", y_expected)
