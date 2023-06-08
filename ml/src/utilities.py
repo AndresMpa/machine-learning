d054ae788a2863287b80a4fc8262fe326e64c939
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-08 16:04:04 -0500

    Adding a file for utilities

---
+++ ml/src/utilities.py
@@ -0,0 +1,64 @@
+import numpy as np
+import codecs
+import yaml
+import json
+import os
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/"
+
+'''
+Creates a directory if it doesn't exist
+'''
+
+
+def handle_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
+
+'''
+Returns a list of params from params.yml
+'''
+
+
+def list_param():
+    params = yaml.safe_load(open(ROOT_PATH + "params.yaml"))
+    for param in params:
+        print(f"{param}: {params[param]}")
+
+
+'''
+Get a param value
+'''
+
+
+def get_params(request):
+    param = yaml.safe_load(open(ROOT_PATH + "params.yaml"))[request]
+    return param
+
+
+'''
+Write a file in the given path
+'''
+
+
+def write_file(raw_content, path, name="file.json"):
+    handle_directory(path)
+
+    content = json.dumps(raw_content, indent=4)
+
+    with open(f"{path}{name}", "w") as outfile:
+        outfile.write(content)
+
+
+'''
+Read a Json like file
+'''
+
+
+def read_numpy_like_json(file_path):
+    content = codecs.open(file_path, 'r', encoding='utf-8').read()
+    raw_content = json.loads(content)
+    array = np.array(raw_content)
+
+    return array
